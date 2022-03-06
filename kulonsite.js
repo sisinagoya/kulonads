13fedabe398b912dfb5f6cf539d5578c7778c164
sisinagoya <68303426+sisinagoya@users.noreply.github.com>
2022-03-06 19:17:37 +0700

    Update kulonsite.js

--- kulonsite.js
+++ kulonsite.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*7)+1);var xxx=null;if(x=="1"){xxx="rool.html"}
+{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*8)+1);var xxx=null;if(x=="1"){xxx="rool.html"}
 if(x=="2"){xxx="wall.html"}
 if(x=="3"){xxx="dress.html"}
 if(x=="4"){xxx="silk.html"}
